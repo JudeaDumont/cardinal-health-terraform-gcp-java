326173c363f92bbfb119a32c416d6867527edb61
dumont, judea <dumontjudea2@gmail.com>
2024-09-14 10:12:31 -0700

    removed unnecessary testcontainers dependency and references

--- services/simpleService/src/test/java/com/example/simpleService/integration/TestCalculationController.java
+++ services/simpleService/src/test/java/com/example/simpleService/integration/TestCalculationController.java
@@ -1,22 +1,17 @@
 package com.example.simpleService.integration;
 
-import com.example.simpleService.model.InputObject;
-import com.example.simpleService.model.OutputObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 @AutoConfigureMockMvc
